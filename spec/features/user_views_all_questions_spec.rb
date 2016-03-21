5606b90f626127de33a9139be7837a4588fe9162
salisbum <megan.a.salisbury@gmail.com>
2016-03-21 17:28:33 -0400

    testing and functionality for viewing questions index

--- spec/features/user_views_all_questions_spec.rb
+++ spec/features/user_views_all_questions_spec.rb
@@ -10,5 +10,14 @@ require 'rails_helper'
 # - I must see questions listed in order, most recently posted first
 
 feature 'all questions are listed on index page' do
+  scenario 'user views index page of questions' do
+    Question.create(title: 'How do I derp?', description: 'I really want to know how to derp. I really want to know how to derp.I really want to know how to derp.I really want to know how to derp.I really want to know how to derp.')
 
+    Question.create(title: 'How do I HERP?', description: 'I really want to know how to herp. I really want to know how to herp.I really want to know how to herp.I really want to know how to herp.I really want to know how to herp.')
+
+    visit '/questions'
+
+    expect(page).to have_content ('How do I derp?')
+    expect(page).to have_content ('How do I HERP?')
+  end
 end
