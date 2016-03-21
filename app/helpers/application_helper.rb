9e100ce049b631c974c41129a4cd56799d31a936
salisbum <megan.a.salisbury@gmail.com>
2016-03-21 14:16:11 -0400

    initial oauth setup and css for login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # helpers do
+  #   def current_user
+  #     if @current_user.nil? && session[:user_id]
+  #       @current_user = User.find_by(id: session[:user_id])
+  #       session[:user_id] = nil unless @current_user
+  #     end
+  #     @current_user
+  #   end
+  # end
+
 end
