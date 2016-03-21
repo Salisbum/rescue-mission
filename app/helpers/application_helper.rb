3429a5edb375fe93d1dca52fcf2549a6d8a097ac
salisbum <megan.a.salisbury@gmail.com>
2016-03-21 14:22:27 -0400

    Updated auth files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 module ApplicationHelper
 
-  # helpers do
-  #   def current_user
-  #     if @current_user.nil? && session[:user_id]
-  #       @current_user = User.find_by(id: session[:user_id])
-  #       session[:user_id] = nil unless @current_user
-  #     end
-  #     @current_user
-  #   end
-  # end
-
 end
