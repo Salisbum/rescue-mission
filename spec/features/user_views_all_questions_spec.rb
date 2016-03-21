56725bb94cd8aa30e8c88d77409fff47284556d2
salisbum <megan.a.salisbury@gmail.com>
2016-03-21 15:07:23 -0400

    Initial testing framework

---
+++ spec/features/user_views_all_questions_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+# As a user
+# I want to view recently posted questions
+# So that I can help others
+
+# Acceptance Criteria
+
+# - I must see the title of each question
+# - I must see questions listed in order, most recently posted first
+
+feature 'all questions are listed on index page' do
+
+end
