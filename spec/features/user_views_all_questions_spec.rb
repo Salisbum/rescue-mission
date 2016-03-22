78dbb5ba9f65c9d4ae1540b00e881f07eca85607
salisbum <megan.a.salisbury@gmail.com>
2016-03-22 13:32:59 -0400

    Completed tests and code for viewing question details

--- spec/features/user_views_all_questions_spec.rb
+++ spec/features/user_views_all_questions_spec.rb
@@ -15,6 +15,17 @@ feature 'all questions are listed on index page' do
 
     Question.create(title: "How do I do the HERP?", description: "Is HERP similar to DERP? what's the difference? AGAIN WITH THE MEME's guys. I see all these images online, including for people like Derpina and Herpina, and it's all so confusing. HOW Is IT RELATED TO DERP?")
 
+    visit '/questions'
+
+    expect(page).to have_content ("How do I do the derp?")
+    expect(page).to have_content ("How do I do the HERP?")
+  end
+
+  scenario 'user views index with multiple pages of questions' do
+    Question.create(title: "How do I do the derp?", description: "I really want to know what 'derp' means and how one does it. It is just a simple error? Do I make an easy mistake? Why is it often 'Derp' and used as a name? I don't understand that meme at all, guys. HALP.")
+
+    Question.create(title: "How do I do the HERP?", description: "Is HERP similar to DERP? what's the difference? AGAIN WITH THE MEME's guys. I see all these images online, including for people like Derpina and Herpina, and it's all so confusing. HOW Is IT RELATED TO DERP?")
+
     Question.create(title: "What is the recipe for a peanut butter jelly sandwich?", description: "I really want to know how to make a sandwich, guys. I've been told that a peanut butter and jelly, or what you might call a 'pb&j', is the easiest way to go when you just start.")
 
     Question.create(title: "How do I choose a tasty beer?", description: "Do I choose the beer based on hops? based on malt? what are these words, even? All I've ever had to drink is budweiser but my friends tell me that that's really bad.")
@@ -26,5 +37,8 @@ feature 'all questions are listed on index page' do
     expect(page).to have_content ("How do I do the derp?")
     expect(page).to have_content ("How do I do the HERP?")
     expect(page).to_not have_content ("What's the best way to eat a mini-egg?")
+
+    click_link "2"
+    expect(page).to have_content ("What's the best way to eat a mini-egg?")
   end
 end
