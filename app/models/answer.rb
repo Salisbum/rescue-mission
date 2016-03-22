a5589835542fc41f45f86419b5aacf224d5e3d0f
salisbum <megan.a.salisbury@gmail.com>
2016-03-22 16:48:12 -0400

    refactored minor

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,6 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   validates :description, presence: true, length: {minimum: 50}
-  validates :question_id, presence: true
+  validates :question, presence: true
 
 end
