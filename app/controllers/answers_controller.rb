9d10a10b9adc3a5007f0cb605fd599393c23bf04
salisbum <megan.a.salisbury@gmail.com>
2016-03-23 12:47:16 -0400

    Refined Answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,16 +1,5 @@
 
 class AnswersController < ApplicationController
-
-  def index
-    @question = Question.find(params[:question_id])
-    @answers =  @question.answers
-  end
-
-  def new
-    @question = Question.find(params[:question_id])
-    @answer =  Answer.new
-  end
-
   def create
     @question = Question.find(params[:question_id])
     @answers = @question.answers.order(created_at: :asc)
